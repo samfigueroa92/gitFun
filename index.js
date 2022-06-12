72263bebb788eae468ea49041fa8f1120cb42771
Samantha Figueroa <samanthafigueroa@pursuit.org>
2022-06-12 10:29:52 -0400

    changes variable name

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function createPerson(name, age) {
     };
   }
   
-  function addPersonAsFriend(person, other) {
-    person.friends.push(other);
+  function addPersonAsFriend(personA, friend) {
+    personA.friends.push(friend);
     return person;
   }
   
